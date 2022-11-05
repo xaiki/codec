1fc79d72de2aa1beff8f8c6156f13a79f66bc2dc
Niv Sardi <xaiki@evilgiggle.com>
2022-11-05 19:56:24 -0300

    rewrite functions/googlesheets for clarity and extensability
    we split the code in functions/googlesheets into a handlers for clarity,
    we also:
     - switch from array.forEach() to for (element of array) that are easier to
     optimize
     - simplify code, we count things instead of building object to then count
     them, we parse most request elements and sheetids to a config element that we
     pass around
    
    this makes the whole file easier to grasp, as we will soon introduce more
    methods here.

--- functions/googlesheets.js
+++ functions/googlesheets.js
@@ -10,83 +10,99 @@ const getDoc = async () => {
         client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
         private_key: process.env.GOOGLE_CLIENT_PRIVATE_KEY.replace(/\\n/gm, '\n')
     });
-    await unready_doc.loadInfo()
-    global_doc = unready_doc
-    return global_doc
+    await unready_doc.loadInfo();
+    global_doc = unready_doc;
+    return global_doc;
+}
+
+const getPlatformConfig = async (doc, { offset, title_to_id }) => {
+    const index = title_to_id['Platform config'];
+    const rows = await doc.sheetsByIndex[index].getRows({ offset });
+    const platform_config = {};
+    for (row of rows) {
+        platform_config[row._rawData[0]] = row._rawData[1];
+    }
+    return platform_config;
+}
+
+const getSize = async (doc, { tab, offset, title_to_id }) => {
+    const list_sheet_index = title_to_id[tab];
+    //ensure an non empty columns exists after the last filled column in that top row
+    const sheet_rows = await doc.sheetsByIndex[list_sheet_index].getRows({ offset });
+    let n_rows = 0;
+    let total_size = 0;
+    for (row of sheet_rows) {
+        if (row._rawData[0] !== '') {
+            total_size += row._rawData.length; //.reduce((a, c) => a + length(c), 0);
+            n_rows++;
+        }
+    }
+    return {
+        total_size,
+        n_rows
+    }
+
+}
+
+const getTab = async (doc, { tab, range, offset, title_to_id }) => {
+    const list_sheet_index = title_to_id[tab];
+    //ensure an non empty columns exists after the last filled column in that top row
+    let sheet_rows;
+    if (range.start && range.end) {
+        sheet_rows = await doc.sheetsByIndex[list_sheet_index].getRows({
+            offset: offset + range.start,
+            limit: range.end - range.start
+        })
+    } else {
+        sheet_rows = await doc.sheetsByIndex[list_sheet_index].getRows({ offset });
+    }
+    let items = [];
+    for (row of sheet_rows) {
+        if (row._rawData[0] !== '') {
+            items.push(row._rawData);
+        }
+    }
+    return items;
+}
+
+const handlers = {
+    platformconfig: getPlatformConfig,
+    size: getSize,
+    tab: getTab
 }
 
 exports.handler = async (event, context, callback) => {
     try {
-        const doc = await getDoc()
-        const request = event.queryStringParameters.request
-        const requested_sheet_offset = parseInt(event.queryStringParameters.offset) - 2
-        const requested_sheet_range_start = parseInt(event.queryStringParameters.rangeStart)
-        const requested_sheet_range_end = parseInt(event.queryStringParameters.rangeEnd)
-
-        let sheet_ids_by_title = {}
-        Object.keys(doc._rawSheets).forEach((key) => {
-            let sheet = doc._rawSheets[key]
-            sheet_ids_by_title[sheet._rawProperties.title] = sheet._rawProperties.index
-        })
-
-
-        if (request === 'platformconfig') {
-            let platform_config_rows = await doc.sheetsByIndex[sheet_ids_by_title['Platform config']].getRows({ offset: requested_sheet_offset })
-            let platform_config = {}
-            platform_config_rows.forEach((platform_config_row) => {
-                platform_config[platform_config_row._rawData[0]] = platform_config_row._rawData[1]
-            })
-            return {
-                statusCode: 200,
-                body: JSON.stringify(platform_config),
-    }
-        } else if (request.includes('size')) {
-            let requested_sheet = request.slice(5)
-            const list_sheet_index = sheet_ids_by_title[requested_sheet]
-    //ensure an non empty columns exists after the last filled column in that top row
-            let sheet_rows = await doc.sheetsByIndex[list_sheet_index].getRows({ offset: requested_sheet_offset })
-            let items = []
-            sheet_rows.forEach((sheet_row) => {
-                if (sheet_row._rawData[0] !== '') {
-                    items.push(sheet_row._rawData)
-        }
-            })
-            let assets_info = {
-                total_size: Buffer.byteLength(JSON.stringify(items)),
-                n_rows: items.length,
-    }
-    return {
-                statusCode: 200,
-                body: JSON.stringify(assets_info),
-    }
-        } else {
-            const tab = event.queryStringParameters.tab;
-            const list_sheet_index = sheet_ids_by_title[tab];
-    //ensure an non empty columns exists after the last filled column in that top row
-            let sheet_rows
-            if (requested_sheet_range_end && requested_sheet_range_end) {
-                sheet_rows = await doc.sheetsByIndex[list_sheet_index].getRows(
-                    {
-                        offset: requested_sheet_offset + requested_sheet_range_start,
-                        limit: requested_sheet_range_end - requested_sheet_range_start
-        })
-
-    } else {
-                sheet_rows = await doc.sheetsByIndex[list_sheet_index].getRows({ offset: requested_sheet_offset })
-    }
-            let items = []
-            sheet_rows.forEach((sheet_row) => {
-                if (sheet_row._rawData[0] !== '') {
-                    items.push(sheet_row._rawData)
-        }
-            })
-        return {
-            statusCode: 200,
-                body: JSON.stringify(items),
+        const doc = await getDoc();
+        const { offset, rangeStart, rangeEnd, ...rest } = event.queryStringParameters;
+        const config = {
+            offset: parseInt(offset) - 2,
+            range: {
+                start: parseInt(rangeStart),
+                end: parseInt(rangeEnd)
             }
         }
+
+        for ([k, v] of Object.entries(rest)) {
+            config[k] = v;
+        }
+
+        config.title_to_id = {};
+        for (key of Object.keys(doc._rawSheets)) {
+            let { title, index } = doc._rawSheets[key]._rawProperties;
+            config.title_to_id[title] = index;
+        }
+
+        const ret = await handlers[config.request](doc, config);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(ret)
+        }
     } catch (err) {
-        console.log(err)
-        return { statusCode: 500, body: err.toString() }
+        console.log('googlesheets failed', err)
+        return {
+            statusCode: 500,
+            body: err.toString()
+        }
     }
 }
