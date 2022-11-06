57e1e4407025265878115da85b4c0ddea7e7470a
Niv Sardi <xaiki@evilgiggle.com>
2022-11-06 11:46:16 -0300

    MediaForm: edit content analysis data from within the MEDIA tab
    create a new component: MediaForm.svelte to make use of the contentAnalysis
    object showing a form that dynamically depends on the
    CONTENT_ANALYSIS_FIRST_COLUMN and subsequent in the sheet,
    add columns, and new options will show,
    we currently auto-detect boolean and numbers and will default to a text entry
    for anything else, but new field types can easily be added.

---
+++ src/components/modules/MediaForm.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+  import {media_store} from '../../stores/store.js';
+  export let medium;
+  let expanded = false
+</script>
+
+<div class={`media-form-container ${expanded?"expanded":""}`}>
+    <div class="module_topbar" on:click={() => {expanded = !!!expanded}}>
+      <div class="module_title text_level1"><div class="arrow">«</div>Options</div>
+    </div>
+    <div class="media-form-content">
+        <form on:change={e => media_store.push(medium)}>
+        {#each Object.entries(medium.contentAnalysis) as [key, value]}
+            <div class="media-form-item">
+                {#if typeof value == "boolean"}
+                    <input name={key} type="checkbox" bind:checked={medium.contentAnalysis[key]}/>
+                    <label class="label_text" for={key}>{key}</label>
+            {:else if typeof(value) == "number"}
+                    <label class="label_text" for={key}>{key}</label>
+                    <input name={key} type="number" bind:value={medium.contentAnalysis[key]}
+                    />
+                {:else if key[0] !== '_'}
+                    <h3 class="label_text" for={key}>{key}</h3>
+                    <textarea name={key} rows=3 bind:value={medium.contentAnalysis[key]}
+                          placeholder="describe what you see in the image in a short sentence" />
+            {/if}
+          </div>
+        {/each}
+      </form>
+      <slot></slot>
+    </div>
+</div>
+
+<style>
+ .media-form-container {
+   display: flex;
+   flex-direction: column;
+   background-color: white;
+   direction: rtl; /* ladies and gents: DA HACK */
+   width: 20px;
+
+   overflow: visible;
+   margin: 10px;
+   border-radius: 7px;
+   transition: all 200ms;
+ }
+ .media-form-container.expanded {
+     width: 20%;
+ }
+ .media-form-content {
+     width: 100%;
+     display:flex;
+     direction: ltr;
+     flex-direction: column;
+     opacity: 0;
+ }
+ .expanded .media-form-content {
+     opacity: 1;
+ }
+ .module_topbar {
+     display: flex;
+     margin: 5px;
+     justify-content: flex-start;
+ }
+ .module_topbar .module_title {
+     display: flex;
+     font-size: 16px;
+     transform: translateX(30px) translateY(25px) rotate(90deg);
+     transition: all 200ms;
+ }
+
+ .expanded .module_topbar .module_title {
+   transform: rotate(0deg);
+ }
+
+ .arrow {
+     position: relative;
+     float: right;
+     transform: rotate(-90deg);
+ }
+ .expanded .arrow {
+     transform: rotate(-180deg);
+ }
+
+ .media-form-item {
+     padding: 5px;
+ }
+
+ label {
+     padding: 5px;
+     padding-left: 20px;
+ }
+
+ textarea {
+     margin-left: 20px;
+     height: 50px;
+     border: 1px solid;
+ }
+
+ textarea::placeholder {
+     color: lightgray;
+ }
+</style>
